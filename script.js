aa510a7e7f3f5c6d86bcce984d48bbc714f3eb0f
FRANCO1357 <fmelani13@gmail.com>
2022-07-05 16:06:28 +0200

    milestone 3

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // 2. Stampare in console i dati 
 // 3. Stampare le stesse informazioni su DOM sottoforma di stringhe
 
+const team = document.getElementById('team');
+
 const teamMembers = [
     { name: 'Wayne Barnett', role: 'Founder & CEO', img: 'wayne-barnett-founder-ceo.jpg', },
     { name: 'Angela Caroll', role: 'Chief Editor', img: 'angela-caroll-chief-editor.jpg', },
@@ -11,10 +13,13 @@ const teamMembers = [
     { name: 'Barbara Ramos', role: 'Graphic Designer', img: 'barbara-ramos-graphic-designer.jpg', },
 ]
 
-
+let info = '';
 
 for (let i = 0; i < teamMembers.length; i++){
     console.log(teamMembers[i].name);
     console.log(teamMembers[i].role);
     console.log(teamMembers[i].img);
+    info += `<div><img src="img/${teamMembers[i].img}" alt=""><h4>${teamMembers[i].name}</h4><p>${teamMembers[i].role}</p></div>`
 }
+
+team.innerHTML = info;
